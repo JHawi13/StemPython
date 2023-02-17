4f6d88cba70a9de3eb8bcd8bd03c544dce9ede10
JHawi13 <Joherwi@gmail.com>
2023-02-17 10:42:59 +0300

    some more work on dictionaries

--- areaofacircle.py
+++
@@ -1,5 +0,0 @@
-#calculating area of a circle 
-radius=12
-pi=3.14
-area=pi*radius*radius
-print(area)
--- fun.py
+++ fun.py
@@ -1,2 +1,19 @@
-for X in range(1,8,1):
-    print("Hawi"*X)
+profile={}
+def register():
+    Username=input("enter username:")
+    Email=input("Enter Email:")
+    password=input("Enter password")
+
+    profile["username"]=Username
+    profile["email"]=Email
+    profile["Password"]=password
+def get_profile ():
+    print(profile)
+register()
+get_profile()
+
+def change_username():
+    new_name=input("Enter your new username")
+    profile["username"]=new_name
+change_username()
+get_profile()
